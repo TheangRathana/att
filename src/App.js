b2482e298b7de36ff689377db7fd57b6970a094b
Rathnana <theangrathana1@gmail.com>
2023-11-23 10:32:40 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { request, gql } from 'graphql-request';
 
 function App() {
+  const [isChecked, setIsChecked] = useState(true);
+  const [data, setData] = useState(null);
+
+  const handleToggle = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const containerStyle = {
+    marginTop: 80,
+    display: 'flex',
+    justifyContent: 'center',
+    height: '100vh', // Adjust the height as needed
+  };
+  const contentStyle = {
+    // Additional styles for your content
+  };
+
+  const handleUpdate = async () => {
+    const endpoint = 'http://localhost:2400/graphql'; // Replace with your GraphQL endpoint
+
+    // Define the GraphQL mutation
+    const mutation = gql`
+     mutation CheckAtt($shift: String!) {
+  checkAtt(shift: $shift) {
+    status
+    date
+  }
+}
+    `;
+
+    // Set the variables for the mutation
+    const variables = {
+      shift: isChecked ? 'morning' : 'afternoon'
+    };
+
+    try {
+      // Perform the GraphQL mutation
+      const result = await request(endpoint, mutation, variables);
+      console.log('Updated user:', result);
+      setData(result)
+    } catch (error) {
+      console.error('Error updating user:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={containerStyle}>
+      <div style={contentStyle}>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={handleToggle}
+            />
+            Toggle Switch
+          </label>
+
+          <p>Switch to {isChecked ? 'Morning Shift' : 'Afternoon Shift'}</p>
+        </div>
+        <div>
+          <button onClick={handleUpdate}>Check Att</button>
+
+        </div>
+        <div>{data?.checkAtt?.status}</div>
+      </div>
     </div>
   );
 }
